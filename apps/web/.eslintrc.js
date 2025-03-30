a5b46dda5824bd9baf0be022492b9dfb5d68d91e
ssdeanx <ssdeanx@gmail.com>
2025-03-30 12:53:06 -0400

    update(deps) updated prettier and installed @mastra/upstash, @mastra/pinecone, @mastra/redis

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,15 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['@repo/eslint-config/next.js'],
+  globals: {
+    React: true,
+    JSX: true,
+  },
   parserOptions: {
     project: true,
   },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  endOfLine: 'lf',
 };
